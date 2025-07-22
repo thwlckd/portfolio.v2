0e17e37608d9aee2357f7f11f46dd7b7e766d8f4
thwlckd <thwlckd@naver.com>
2025-07-22 19:26:33 +0900

    초기 세팅

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+const HomePage = () => {
+  return (
+    <Imsi>
+      <h1 css={{ color: 'blue' }}>hello</h1>
+      <h2>hello</h2>
+      <button>sadf</button>
+    </Imsi>
+  )
+}
+
+export default HomePage
+
+const Imsi = styled.div({
+  color: 'red',
+})
