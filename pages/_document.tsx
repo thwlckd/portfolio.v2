fdbf14a12501d38ea71e02eeb3e66c98dcb1b2e2
thwlckd <thwlckd@naver.com>
2025-08-03 06:13:38 +0900

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const Document = () => {
   return (
@@ -6,6 +7,17 @@ const Document = () => {
       <Head>
         <meta name="description" content="hyub2 archive" />
         <link rel="icon" href="/svg/hyub.svg" />
+
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VBCXVMB45G" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VBCXVMB45G');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
