5596ca511b42272de84209afffda710b0a2d6e1e
thwlckd <thwlckd@naver.com>
2025-08-20 12:31:46 +0900

    log sentry on production

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,15 +4,17 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: 'https://8a97730091194fabf60881ad5c73f6c2@o4509817046106112.ingest.us.sentry.io/4509817047089152',
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://8a97730091194fabf60881ad5c73f6c2@o4509817046106112.ingest.us.sentry.io/4509817047089152',
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
