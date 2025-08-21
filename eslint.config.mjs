e72e233de06762b16f5dd26735c7c709cb80add2
thwlckd <thwlckd@naver.com>
2025-08-22 01:23:46 +0900

    jsx-a11y lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'jsx-a11y/control-has-associated-label': 'warn',
+    },
+  },
+];
 
 export default eslintConfig;
