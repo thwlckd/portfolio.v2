ae1279835aaaad7660f86d6491223bad60b8a78f
thwlckd <thwlckd@naver.com>
2025-09-16 23:58:57 +0900

    도커라이징

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'standalone',
 };
 
 export default withSentryConfig(nextConfig, {
